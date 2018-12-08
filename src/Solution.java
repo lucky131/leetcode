7baadca564ce20c1f4e3ae21f8eed8658fc16240
lucky131 <542723049@qq.com>
2018-12-08 23:42:48 +0800

    init leetcode

---
+++ src/Solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+
+    // 771. Jewels and Stones
+    // https://leetcode.com/problems/jewels-and-stones/
+    public int numJewelsInStones(String J, String S) {
+
+        return 111;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.numJewelsInStones("", ""));
+    }
+
+}
